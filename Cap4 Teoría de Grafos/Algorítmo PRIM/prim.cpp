5e1e2ae3d5932742261bb959b441e3b1053f14e5
PabloAcker <90888080+PabloAcker@users.noreply.github.com>
2022-10-22 19:58:14 -0400

    Update prim.cpp

--- Cap4 Teoría de Grafos/Algorítmo PRIM/prim.cpp
+++ Cap4 Teoría de Grafos/Algorítmo PRIM/prim.cpp
@@ -2,45 +2,65 @@
 #define input freopen("in.txt", "r", stdin)
 #define output freopen("out.txt", "w", stdout)
 using namespace std;
-vector<pair<int, int> > grafo[100000];
+vector< pair <int,pair<int, int>> > grafo[100000];
+vector< pair <int,pair<int, int>> > grafoMST[100000];
 bool visitados[100000];
 
 int Prim(int verticeInicial){
-    multiset<pair<int, int>> colaPrioridad;
-    colaPrioridad.insert(make_pair(0, verticeInicial)); 
-    int answer = 0;
+    multiset< pair<int,pair<int, int>> > colaPrioridad;
+    colaPrioridad.insert(make_pair(0, make_pair(verticeInicial,verticeInicial))); //cola de prioridad de parejas de enteros (-distancia del nodo a F, nodo)
+    int expansionMST = 0;
+
     while (!colaPrioridad.empty()){
-        pair<int, int> verticeActual = *colaPrioridad.begin();
-        colaPrioridad.erase(colaPrioridad.begin()); 
+        pair <int,pair<int, int>> verticeActual = *colaPrioridad.begin();
+        colaPrioridad.erase(colaPrioridad.begin()); //lo quitamos de la cola
 
-        int vertice = verticeActual.second; 
-        int peso = verticeActual.first; 
+        pair<int,int> verticeDireccionado = verticeActual.second; //vértice de Q a menor distancia de F
+        int peso = verticeActual.first; //distancia entre F y v
 
-        if (!visitados[vertice]){ 
+        int verticeBegin = verticeDireccionado.first;
+        int vertice = verticeDireccionado.second;
+
+        if (!visitados[vertice]){ //si no lo hemos visitado
             visitados[vertice] = true;
-            answer += peso;
-            for (int i = 0; i < grafo[vertice].size(); ++i){ 
-                int verticeVecino = grafo[vertice][i].second;
+            expansionMST += peso;
+            
+            grafoMST[verticeBegin].push_back(make_pair(peso,make_pair(verticeBegin,vertice)));
+            /*aca se crea el nuevo grafo
+            
+            */
+            for (int i = 0; i < grafo[vertice].size(); ++i){ //miramos sus vecinos
+                pair<int, int> verticeVecino = grafo[vertice][i].second;
                 int pesoVecino = grafo[vertice][i].first;
-                colaPrioridad.insert(make_pair(pesoVecino, verticeVecino)); 
+                colaPrioridad.insert(make_pair(pesoVecino, verticeVecino)); // añadimos los vecinos conectados con u
             }
         }   
     }
 
-    return answer; 
+    return expansionMST; // devolvemos el coste
 }
+
+
 
 int main()
 {
+    input;
     int vertices, aristas;
     cin >> vertices >> aristas;
     for (int i = 0; i < aristas; i++)
     {
         int ini,fin,peso; 
         cin>>ini>>fin>>peso;
-        grafo[ini].push_back(make_pair(peso,fin));
-        grafo[fin].push_back(make_pair(peso,ini));
+        grafo[ini].push_back(make_pair(peso,make_pair(ini,fin)));
+        grafo[fin].push_back(make_pair(peso,make_pair(fin,ini)));
     }
     cout <<  Prim(0) << endl;
+
+    for (int i = 0; i < vertices; i++){
+        for(int j = 0 ;j < grafoMST[i].size();j++){
+            cout<<grafoMST[i][j].first<<" "<<grafoMST[i][j].second.first<<" "<< grafoMST[i][j].second.second<<endl;
+        }
+    }
+
     return 0;
 }
